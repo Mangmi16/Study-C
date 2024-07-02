9908f6164941d6d31c227d77b473c583f4798049
Mangmi16 <95905822+Mangmi16@users.noreply.github.com>
2024-07-02 17:37:13 +0200

    Add files via upload

---
+++ condition.c
@@ -0,0 +1,86 @@
+﻿#include <stdio.h>
+int main_condition(void)
+{
+	//Distinguish between student rates and regular rates.
+	/*int age ;
+	printf("Please enter your age.");
+	scanf_s("%d", &age);
+
+	if (age >= 20)
+		printf("adult");
+	else
+		printf("student");*/
+
+	//age(8~13)Elementary  student / age (14~16) Middle schooler / age(17~19) High School student
+
+	/*int age;
+	printf("Please enter your age.");
+	scanf_s("%d", &age);
+	if (age >= 8 && age <= 13)
+	{
+		printf("Elementary  student");
+
+	}
+	else if (age >= 14 && age <= 16)
+	{
+		printf("Middle schooler");
+	}
+	else if (age >= 17 && age <= 19)
+	{
+		printf("High School student");
+	}*/
+
+
+	//break
+
+
+	/*for (int i = 1; i <= 30; i++)
+	{
+		if (i >= 6)
+		{
+			printf("The rest of the students should go home.");
+			break;
+		}
+		printf("Student number %d is ready to present.\n", i);
+	}*/
+
+	//There are numbers 1 to 30. Except number 7
+	//From 6 to 10
+	//continue
+	/*for (int i = 1; i <= 30; i++)
+	{
+		if (i >= 6 && i <= 10)
+		{
+			if (i == 7)
+			{
+				printf("Except number 7\n");
+				continue; //NEXT 
+
+			}
+			printf("Student number %d is ready to present.\n", i);
+		}
+	}*/
+
+	// and
+	// 
+// and(&&)  or(||)
+
+	/*int a = 10;
+	int b = 12;
+	int c = 13;
+	int d = 14;
+	if (a == b || c == d)
+	{
+		printf("The nummer of a and b or c and c are the same.\n");
+	}
+	else	
+	{
+	    printf("The nummber are different.");
+	}*/
+
+
+	
+
+
+	return 0;
+}
